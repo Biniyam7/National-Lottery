ecb8ed6ce00bb9ce1d5be0bf68d0c77b41e29839
Teklee <kassateklee@gmail.com>
2023-12-06 11:05:42 +0300

    add admin model

--- models/prize.js
+++ models/prize.js
@@ -2,14 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const prizeSchema = new Schema({
-  level: {
-    type: Number,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
+  prize: [
+    {
+      level: {
+        type: Number,
+        required: true,
+      },
+      amount: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+
   lottery: {
     type: Schema.Types.ObjectId,
     ref: "Lottery",
